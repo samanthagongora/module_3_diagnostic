ff70f691630cfb9acc2564b39f7e5db78d6d7e49
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-30 10:39:53 -0600

    adds nrel service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @stations = NrelService.new(params[:q])
-    zip_code = params[:q]
-    conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1.json') do |faraday|
-      faraday.adapter  Faraday.default_adapter
-    end
-
-    response = conn.get "/nrel/alt-fuel-stations/v1.json", { :api_key => "#{ENV['nrel_key']}", :zip => "#{zip_code}", :radius => "6.0", :fuel_type => "ELEC, LNG", :limit => '10' }
-
-    results = JSON.parse(response.body)
-byebug
-    @stations = results[:fuel_stations]
+    byebug
+    @stations = NrelService.find_stations(params[:q])["fuel_stations"]
   end
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,12 +1,22 @@
 class NrelService
-  zip_code = params[:q]
-  conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1.json') do |faraday|
-    faraday.adapter  Faraday.default_adapter
+  def initialize(zip_code)
+    @zip_code = zip_code
+    @conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/v1.json') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 
-  response = conn.get "/nrel/alt-fuel-stations/v1.json", { :api_key => "#{ENV['nrel_key']}", :zip => "#{zip_code}", :radius => "6.0", :fuel_type => "ELEC, LNG", :limit => '10' }
+  def find_nearest_stations
+    response = @conn.get "/nrel/alt-fuel-stations/v1.json",
+    { :api_key => "#{ENV['nrel_key']}",
+      :zip => "#{@zip_code}",
+      :radius => "6.0",
+      :fuel_type => "ELEC, LNG",
+      :limit => '10' }
+    results = JSON.parse(response.body)["fuel_stations"]
+  end
 
-  results = JSON.parse(response.body)
-  byebug
-  @stations = results[:fuel_stations]
+  def self.find_stations(zip_code)
+    new(zip_code).find_nearest_stations
+  end
 end
